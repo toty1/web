f747217f629907a7fbd0c8196dfaded65a92dc5d
Buzaki <admin@id3m.net>
2016-10-23 16:46:19 +0530

    init commit

---
+++ 3-j.s/script.js
@@ -0,0 +1,61 @@
+ var start = new Date().getTime();
+        
+            function getRandomcolor(){
+            
+            var letters = '0123456789ABCDEF'.split('');
+            
+            var color = '#';
+            
+            for (var i = 0; i < 6; i++) {
+                
+             color += letters[Math.floor(Math.random() * 16)];   
+                
+            }
+            
+            return color
+        }
+        
+         function makeShapeAppear() {
+             
+         var top = Math.random() * 400;
+        
+         var left = Math.random() * 400;
+             
+         var width = Math.random() * 300;
+             
+         document.getElementById("shape").style.backgroundColor = getRandomcolor();
+              
+          document.getElementById("shape").style.top = top + "px";
+             
+          document.getElementById("shape").style.left = left + "px";
+    
+          document.getElementById("shape").style.display = "block";
+          
+          start = new Date().getTime();
+            
+          
+         }
+        
+         function appearAfterDelay() {
+             
+              setTimeout(makeShapeAppear , Math.random() *2000);
+             
+         }
+        
+          appearAfterDelay();
+        
+           var start = new Date().getTime();
+      
+        document.getElementById("shape").onclick = function() {
+              
+         document.getElementById("shape").style.display = "none";
+            
+            var end = new Date().getTime();
+            
+            var timeTaken = (end - start) / 1000;
+            
+         document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+            
+            appearAfterDelay();
+        
+        }
